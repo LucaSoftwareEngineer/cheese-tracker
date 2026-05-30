0a97b0be8501abe2c96d4e4001f4225738594e33
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-05-30 20:57:46 +0200

    init backend

---
+++ cheese-tracker-backend/src/main/java/com/github/lucasoftwareengineer/CheeseTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasoftwareengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ cheese-tracker-backend/src/test/java/com/github/lucasoftwareengineer/CheeseTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasoftwareengineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
